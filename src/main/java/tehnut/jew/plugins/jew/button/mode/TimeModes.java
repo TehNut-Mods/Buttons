f583303514560c504ba3b38274e85502e1731558
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-07-11 13:37:25 -0700

    Time should increment globally instead of per-dimension

--- src/main/java/tehnut/jew/plugins/jew/button/mode/TimeModes.java
+++ src/main/java/tehnut/jew/plugins/jew/button/mode/TimeModes.java
@@ -4,6 +4,8 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import tehnut.jew.api.WidgetTexture;
@@ -14,7 +16,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Locale;
 
-public enum  TimeModes implements IMode {
+public enum TimeModes implements IMode {
 	DAWN(WidgetTextures.DAWN, 23000),
 	DAY(WidgetTextures.DAY, 6000),
 	DUSK(WidgetTextures.DUSK, 13000),
@@ -44,16 +46,18 @@ public enum  TimeModes implements IMode {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public EnumActionResult onClientClick(int mouseX, int mouseY) {
-		return net.minecraft.util.EnumActionResult.SUCCESS;
+		return EnumActionResult.SUCCESS;
 	}
 
 	@Override
 	public void onServerClick(EntityPlayerMP player) {
-		int ticksPerDay = 24000;
-		long currentTime = player.getEntityWorld().getWorldTime();
-		long currentDayTime = currentTime % ticksPerDay;
-		long targetDayTime = time % ticksPerDay;
-		long diffTime = ((targetDayTime - currentDayTime) + ticksPerDay) % ticksPerDay;
-		player.getEntityWorld().setWorldTime(currentTime + diffTime);
+		for (WorldServer worldServer : FMLCommonHandler.instance().getMinecraftServerInstance().worldServers) {
+			int ticksPerDay = 24000;
+			long currentTime = worldServer.getWorldTime();
+			long currentDayTime = currentTime % ticksPerDay;
+			long targetDayTime = time % ticksPerDay;
+			long diffTime = ((targetDayTime - currentDayTime) + ticksPerDay) % ticksPerDay;
+			worldServer.setWorldTime(currentTime + diffTime);
+		}
 	}
 }
